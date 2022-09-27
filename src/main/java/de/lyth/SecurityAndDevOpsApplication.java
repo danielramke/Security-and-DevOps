84634d62686bab8d45e55393c8c19320cd3fabea
danielramke <genshin.exepta@outlook.com>
2022-09-27 14:05:55 +0200

    jenkins setup test

---
+++ src/main/java/de/lyth/SecurityAndDevOpsApplication.java
@@ -0,0 +1,13 @@
+package de.lyth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAndDevOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityAndDevOpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/lyth/SecurityAndDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lyth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAndDevOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
